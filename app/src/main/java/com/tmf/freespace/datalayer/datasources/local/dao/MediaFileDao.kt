1beddfcd08cfe951e419f76465ec8450558526c2
MikeMayfield <mike@themayfields.com>
2025-09-03 11:44:57 -0700

    Making progress, but may not uploading and compressing properly.

--- app/src/main/java/com/tmf/freespace/datalayer/datasources/local/dao/MediaFileDao.kt
+++ app/src/main/java/com/tmf/freespace/datalayer/datasources/local/dao/MediaFileDao.kt
@@ -1,11 +1,13 @@
 package com.tmf.freespace.datalayer.datasources.local.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.tmf.freespace.datalayer.models.MediaFile
+import com.tmf.freespace.datalayer.models.MediaType
 
 @Dao
 interface MediaFileDao {
@@ -15,25 +17,38 @@ interface MediaFileDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertIfNew(mediaFile: MediaFile)
 
+    /**
+     * Set desired compression level for media files in database based on their creation date range
+     *
+     * <param>mostRecentDtm</param> - Most recent date in range to set compression level for
+     * <param>oldestDtm</param> - Oldest date in range to set compression level for
+     * <param>compressionLevel</param> - Compression level to set
+     * <param>mediaType</param> - Media type to set compression level for
+     */
     @Query(
-        "UPDATE MediaFile SET desiredCompressionLevel = :compressionLevel WHERE creationDtm <= :minDateMs AND creationDtm > :maxDateMs AND mediaType = :mediaType AND currentCompressionLevel < :compressionLevel"
+        "UPDATE MediaFile SET desiredCompressionLevel = :compressionLevel" +
+                " WHERE creationDtm > :oldestDtm AND creationDtm <= :mostRecentDtm AND mediaType = :mediaType AND currentCompressionLevel < :compressionLevel"
     )
-    fun setCompressionLevel(minDateMs: Long, maxDateMs: Long, compressionLevel: Int, mediaType: Int)
+    fun setCompressionLevel(mostRecentDtm: Long, oldestDtm: Long, compressionLevel: Int, mediaType: MediaType)
 
-//    @Query("""
-//        SELECT * FROM MediaFile
-//            WHERE currentCompressionLevel != desiredCompressionLevel AND desiredCompressionLevel > 0
-//            ORDER BY desiredCompressionLevel DESC, compressedSize DESC, creationDtm DESC LIMIT 1
-//    """)
-    @Query("""
-        SELECT * FROM MediaFile 
-    """)  //TODO Use code above after testing
-    fun getFileToCompress() : MediaFile?
+    /**
+     * Select the next file to compress. The file will have the highest desired compression level and the largest current (possibly compressed) size
+     */
+    @Query(
+        "SELECT * FROM MediaFile" +
+                " WHERE currentCompressionLevel != desiredCompressionLevel AND desiredCompressionLevel > 0" +
+                " ORDER BY desiredCompressionLevel DESC, compressedSize DESC, creationDtm DESC" +
+                " LIMIT 1"
+    )
+    fun getFileToCompress(): MediaFile?
 
     /**
      * Get media file by ID
+     *
+     * <param>id</param> - ID of media file to get
      */
-    @Query("SELECT * FROM MediaFile WHERE id = :id")
+    @Query("SELECT * FROM MediaFile" +
+            " WHERE id = :id")
     fun getMediaFileByID(id: Long): MediaFile?
 
     /**
@@ -41,4 +56,10 @@ interface MediaFileDao {
      */
     @Update
     fun updateMediaFile(mediaFile: MediaFile)
+
+    /**
+     * Delete media file from database
+     */
+    @Delete()
+    fun deleteMediaFile(mediaFile: MediaFile)
 }
