5450a9c9fc044f811a1c103d52dff86b89872348
MikeMayfield <mike@themayfields.com>
2025-10-16 14:05:57 -0700

    Add SubscriptionPromo screen. Allow Start to go backwards.

--- app/src/main/java/com/tmf/freespace/presentationlayer/ui/navigation/NavRoute.kt
+++ app/src/main/java/com/tmf/freespace/presentationlayer/ui/navigation/NavRoute.kt
@@ -12,29 +12,5 @@ sealed class NavRoute(val path: String) {
     object SubscriptionPromo: NavRoute("subscriptionPromo")
     object Start: NavRoute("start")
     object AppSummary: NavRoute("appSummary")
-
-//    object Search: NavRoute("search") {
-//        val query = "query"
-//    }
-
-    // build navigation path (for screen navigation)
-    fun withArgs(vararg args: String): String {
-        return buildString {
-            append(path)
-            args.forEach{ arg ->
-                append("/$arg")
-            }
-        }
-    }
-
-    // build and setup route format (in navigation graph)
-    fun withArgsFormat(vararg args: String) : String {
-        return buildString {
-            append(path)
-            args.forEach{ arg ->
-                append("/{$arg}")
-            }
-        }
-    }
 }
 
