09d9e3e5edee7ae87a8b59aefb07a7208941fd9d
MikeMayfield <mike@themayfields.com>
2025-10-27 17:07:55 -0700

    Clean up unused methods and data in PropertyBagRepository

--- app/src/main/java/com/tmf/freespace/datalayer/repositories/PropertyBagRepository.kt
+++ app/src/main/java/com/tmf/freespace/datalayer/repositories/PropertyBagRepository.kt
@@ -7,49 +7,11 @@ import com.tmf.freespace.datalayer.models.PropertyBagEntry
 class PropertyBagRepository(val context: Context) {
     private val propertyBagEntryDao = AppDatabase.instance(context).propertyBagEntryDao
 
-    suspend fun allEntries() : List<PropertyBagEntry> {
+    fun allEntries() : List<PropertyBagEntry> {
         return propertyBagEntryDao.getAll()
     }
 
     suspend fun saveBagEntry(propertyBagEntry: PropertyBagEntry) {
         propertyBagEntryDao.upsert(propertyBagEntry)
-    }
-
-
-    /**
-     * Returns the value associated with the given key. If the key is not found, returns null.
-     */
-    suspend fun get(key: String, defaultValue: String = ""): String {
-        loadBag()
-        return bag[key] ?: defaultValue
-    }
-
-    /**
-     * Sets the value associated with the given key.
-     *
-     * @param key The key to associate with the value.
-     * @param value The value to set.
-     */
-    suspend fun set(key: String, value: String) {
-        loadBag()
-        bag[key] = value
-        propertyBagEntryDao.upsert(PropertyBagEntry(key, value))
-    }
-
-
-    /**
-     * Load property bag from database if it is empty.
-     */
-    private suspend fun loadBag() {
-        if (bag.isEmpty()) {
-            for (property in propertyBagEntryDao.getAll()) {
-                bag[property.key] = property.value
-            }
-        }
-    }
-
-
-    companion object {
-        private val bag = mutableMapOf<String, String>()
     }
 }
