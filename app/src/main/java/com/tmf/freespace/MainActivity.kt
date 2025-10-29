95f815f297fdcda8e9180b5f60434b14bd024359
MikeMayfield <mike@themayfields.com>
2025-10-28 20:08:02 -0700

    Resolved TODOs in MainActivity

--- app/src/main/java/com/tmf/freespace/MainActivity.kt
+++ app/src/main/java/com/tmf/freespace/MainActivity.kt
@@ -19,25 +19,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
-//        val permissions = if (Build.VERSION.SDK_INT >= 33) {
-//            arrayOf(
-////                Manifest.permission.READ_MEDIA_AUDIO,
-//                Manifest.permission.READ_MEDIA_VIDEO,
-//                Manifest.permission.READ_MEDIA_IMAGES,
-//            )
-//        } else {
-//            arrayOf(
-//                Manifest.permission.READ_EXTERNAL_STORAGE,
-//                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//            )
-//        }
-//
-//        ActivityCompat.requestPermissions(  //TODO Use user-oriented permission request (see video)
-//            this,
-//            permissions,
-//            0
-//        )
-
         //Display the Welcome or AppSummary screen
         var firstScreenRoute: NavRoute = NavRoute.Welcome
         if (Permissions().allPermissionsAreGranted(this)) {
@@ -48,29 +29,4 @@ class MainActivity : ComponentActivity() {
            MainScreen(viewModel, firstScreenRoute)
         }
     }
-
-//    private fun allPermissionsAreGranted(context: Context, permissions: Array<String>): Boolean {  //TODO REMOVE THIS
-//        for (permission in permissions) {
-//            if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-//                return false
-//            }
-//        }
-//        return false//TODO true  //TODO Change to TRUE to force UI to always start on first screen.
-//    }
-
-//    override fun onRequestPermissionsResult(requestCode: Int, @NonNull permissions: Array<String?>?, @NonNull grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//
-//        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-//            // Check if the request was granted.
-//            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                // Permission was granted, proceed with your app's logic.
-//                openCamera()
-//            } else {
-//                // Permission was denied.
-//                // You may want to show an explanation or disable the related feature.
-//                Toast.makeText(this, "Camera permission was denied.", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    }
 }
