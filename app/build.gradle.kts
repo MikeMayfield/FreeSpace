000914d4610914a449db005e72959ef679438803
MikeMayfield <mike@themayfields.com>
2025-12-27 14:53:47 -0800

    -Update version number to 1.0.13 after publishing prior version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.tmf.freespace"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10012
-        versionName = "1.0.12"
+        versionCode = 10013
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
