1e4d4558eb0e5e0ad88e1c2d31f3f78f5ed64044
MikeMayfield <mike@themayfields.com>
2026-01-13 12:34:45 -0800

    -Display an AlertDialog in case of subscription error instead of a toast.

--- app/src/main/java/com/tmf/freespace/presentationlayer/ui/screens/SubscriptionScreen.kt
+++ app/src/main/java/com/tmf/freespace/presentationlayer/ui/screens/SubscriptionScreen.kt
@@ -1,11 +1,16 @@
 package com.tmf.freespace.presentationlayer.ui.screens
 
-import android.app.Activity
-import android.widget.Toast
 import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import com.tmf.freespace.R
@@ -20,6 +25,30 @@ fun SubscriptionScreen(navController: NavHostController, paddingValues: PaddingV
     val products by viewModel.products.collectAsStateWithLifecycle()
     val subscriptionOfferDetails = products[ProductIds.MONTHLY_200]?.subscriptionOfferDetails
     val monthlyPrice = subscriptionOfferDetails?.firstOrNull()?.pricingPhases?.pricingPhaseList?.firstOrNull()?.formattedPrice ?: "$2.00"
+    var showErrorDialog by remember { mutableStateOf(false) }
+    val activity = LocalActivity.current!!
+
+    if (showErrorDialog) {
+        AlertDialog(
+            onDismissRequest = { showErrorDialog = false },
+            title = {
+                Text(
+                    text = "Subscription Error",
+                    fontWeight = FontWeight.Bold
+                )
+                    },
+            text = { Text("Unable to process your subscription request. You may not have Internet access or your phone may not have Google Play Services installed.") },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        navController.popBackStack()
+                    }
+                ) {
+                    Text("Close")
+                }
+            },
+        )
+    }
 
     GenericTextBody(
         imageID = R.drawable.subscription_promo,
@@ -30,24 +59,13 @@ fun SubscriptionScreen(navController: NavHostController, paddingValues: PaddingV
             "You already get <b>${PropertyBag.getInt(TRIAL_GB_FREE)} GB of extra space for free</b>, but when you’re ready for more, <b>FreeSpace Max</b> gives you automatic expansion and almost <b>limitless</b> memory.",
         navButtonText = "SUBSCRIBE  -  ${monthlyPrice}/Mon",
         paddingValues = paddingValues,
-        onNavButtonClick = handleSubscription(viewModel, LocalActivity.current!!, navController),
+        onNavButtonClick = {
+            if (!viewModel.launchPurchaseFlow(activity, ProductIds.MONTHLY_200)) {
+                showErrorDialog = true
+            }
+            else {
+                navController.popBackStack()
+            }
+        }
     )
-}
-
-private fun handleSubscription(
-    viewModel: CommonViewModel,
-    activity: Activity,
-    navController: NavHostController,
-): () -> Unit = {
-    // Launch the purchase flow for the $2/mon subscription
-    if (!viewModel.launchPurchaseFlow(activity, ProductIds.MONTHLY_200)) {
-        Toast.makeText(activity, "Unable to process your subscription request. You may not have Internet access or your phone may not have Google Play Services installed.", Toast.LENGTH_LONG).show()
-    }
-
-    // The result of the purchase will be handled by the PurchasesUpdatedListener.
-    // We no longer need to manually set the subscription status here.
-    // The popBackStack() should also be moved to the listener's success callback if you
-    // want the screen to close automatically only on a successful purchase.
-    // For now, we can leave it here to close the screen immediately.
-    navController.popBackStack()
 }
